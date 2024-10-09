9eb320f306c30007400df098ccd681d5d2f06743
sandhyasiet <128462987+sandhyasiet@users.noreply.github.com>
2024-10-09 13:23:54 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,19 @@
+provider "google" {
+  project     = "crucial-ray-437907-r9"
+  region      = "us-east1"
+
+}
+
+resource "google_compute_network" "vpc1" {
+  name                    = "my-custom-network-1"
+  auto_create_subnetworks = "false"
+
+}
+
+resource "google_compute_subnetwork" "my-custom-subnet1" {
+  name          = "my-custom-subnet-1"
+  ip_cidr_range = "10.255.196.0/24"
+  network       = google_compute_network.vpc1.name
+  region        = "us-east1"
+}
+
